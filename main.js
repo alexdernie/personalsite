332c46534c83e4116a5b14718a4844eb1303b616
Alex Dernie <alexander.dernie@gmail.com>
2022-06-04 20:05:50 +0100

    typewrite subtitle

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 alert('Hello World');
+
+var i = 0;
+var txt = 'Marketing & Operations Specialist';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("hom-subtitle-text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = typeWriter();
